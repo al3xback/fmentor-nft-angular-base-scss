378a98863cfb2e1e81ca8b912120da38586f5401
alex <al3xback@gmail.com>
2023-10-31 10:56:33 +0700

    fixed minor

--- src/app/main/card/card.component.ts
+++ src/app/main/card/card.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class CardComponent {
 	title = 'Equilibrium #3429';
 	description = 'Our Equilibrium collection promotes balance and calm.';
+	imageSrc = '/assets/images/equilibrium.jpg';
 	ethereumAmount = 0.041;
 	remainingTime = 3;
 	author = {
